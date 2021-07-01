14a57a294c2124062a1517811e8b334d680a8932
nguyenvu2801 <86769475+nguyenvu2801@users.noreply.github.com>
2021-07-01 01:39:53 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 const CONFIG = {
     introTitle: 'Bé iuu à!',
-    introDesc: `anh zới bé cx đc 2 tháng goỳ á 
-    anh iu bé lắmmm lun áaaaaa :> 
-    nên a làm cáiiii nàyyyy(handmade) để bé bic a iu bé nnao 🙄🙄`,
+    introDesc: `Anh zới bé cx đc 2 tháng goỳ á
+    Anh iu bé lắmmm lun áaaaaa :>
+    Nên a làm cáiiii nàyyyy(handmade)
+    để bé bic a iu bé nnao 🙄🙄`,
     btnIntro: '😏🤮',
-    title: 'tháng qua thì có nhiều lúc a làm bé bùn íaaaa thì cho a xin lũiii nheeee🥺🥺',
-    desc: 'nhiều lúc còn như thg điên dở dở hâm hâm nma trong lòng a vx iu bé lắm á😙😙 ',
+    title: 'Tháng qua thì có nhiều lúc a làm bé bùn íaaaa thì cho a xin lũiii nheeee🥺🥺',
+    desc: 'Nhiều lúc còn như thg điên dở dở hâm hâm nma trong lòng a vx iu bé lắm á😙😙 ',
     btnYes: 'uhm vâng😏',
     btnNo: 'khong🙄',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại iu anhhh :>',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'be iu của anh iu anh nhấc màaaaa :>',
-    mess: 'Anh biết có người mún nhắn ghec vs vâng hay gì mà nma hok cóa đc âu hehe🙄🙄 Yêu be nhiều nhiều 😘😘',
-    messDesc: 'iuuuuuuu lắmmmm ạaaaaaa moazzzz😍😍',
-    btnAccept: 'iuuu anh',
+    reply: 'Be iu của anh iu anh nhấc màaaaa :>',
+    mess: 'Anh biết có người mún nhắn ghec vs vâng hay gì mà🙄🙄 nma hok cóa đc âu hehe Yêu be nhiều nhiều 😘😘',
+    messDesc: 'Iuuuuuuu lắmmmm ạaaaaaa moazzzz😍😍',
+    btnAccept: 'Iuuuu anhhh <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
