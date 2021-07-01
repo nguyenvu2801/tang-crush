ed375bb585173f3610cfd3a6046f2aa15a8eee8e
nguyenvu2801 <86769475+nguyenvu2801@users.noreply.github.com>
2021-07-01 01:27:04 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     nên a làm cáiiii nàyyyy(handmade) để bé bic a iu bé nnao 🙄🙄`,
     btnIntro: '😏🤮',
     title: 'tháng qua thì có nhiều lúc a làm bé bùn íaaaa thì cho a xin lũiii nheeee🥺🥺',
-    desc: 'nhiều lúc còn như thg điên dở dở hâm hâm nma trong lòng a vx iu bé lắm á😙😙',
+    desc: 'nhiều lúc còn như thg điên dở dở hâm hâm nma trong lòng a vx iu bé lắm á😙😙 ',
     btnYes: 'uhm vâng😏',
     btnNo: 'khong🙄',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại iu anhhh :>',
