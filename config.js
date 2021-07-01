48c790b61693bf8e2faa77a4c216b78386d0f6b4
nguyenvu2801 <86769475+nguyenvu2801@users.noreply.github.com>
2021-07-01 01:25:57 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     introTitle: 'Bé iuu à!',
     introDesc: `anh zới bé cx đc 2 tháng goỳ á 
     anh iu bé lắmmm lun áaaaaa :> 
-    nên a làm cáiiii nàyyyy(handmade) để bé bic a iu bé nnao 🙄🙄',
+    nên a làm cáiiii nàyyyy(handmade) để bé bic a iu bé nnao 🙄🙄`,
     btnIntro: '😏🤮',
     title: 'tháng qua thì có nhiều lúc a làm bé bùn íaaaa thì cho a xin lũiii nheeee🥺🥺',
     desc: 'nhiều lúc còn như thg điên dở dở hâm hâm nma trong lòng a vx iu bé lắm á😙😙',
